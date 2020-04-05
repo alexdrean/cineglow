9d56b7891f18f01fa28d430bfe3c3957ba1ae4f0
Alexandre <a@alex00.com>
2020-04-04 22:40:00 -0700

    Initial commit

---
+++ src/com/alex00/cineglow/Cineglow.java
@@ -0,0 +1,11 @@
+package com.alex00.cineglow;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Cineglow extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
